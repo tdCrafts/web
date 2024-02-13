80d5d8fab3c6df24d4f32ca95d5d5caaafd0c8ab
Tyler Twining <twijn@twijn.net>
2024-02-13 10:28:17 -0500

    Add base functions in master.js for API requests and status popups

---
+++ static/assets/js/master.js
@@ -0,0 +1,36 @@
+const api = {
+    post: function(url, data, callback) {
+        $.ajax(
+            {
+                method: "POST",
+                url,
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                success: callback,
+            },
+        );
+    }
+}
+
+function statusPopup(type, html, hideAfter = null) {
+    $(".status-popup").remove();
+    const ele = $(`<div class="status-popup alert alert-${type} hide"></div>`);
+    ele.html(html);
+    $("body").append(ele);
+    setTimeout(function() {
+        ele.removeClass("hide");
+    }, 10);
+
+    function hide() {
+        ele.addClass("hide");
+        setTimeout(function() {
+            ele.remove();
+        }, 250);
+    }
+
+    ele.on("click", hide);
+
+    if (hideAfter) {
+        setTimeout(hide, hideAfter);
+    }
+}
